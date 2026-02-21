9cb16b6638371af909831a1dfcb12353df0bcdeb
dzm1098 <91770807+dzm1098@users.noreply.github.com>
2026-02-21 01:19:00 +0000

    Deploying to gh-pages from @ dzm1098/dzm1098.github.io@0f3242b93de9833f7ddc21a5e4ecae8f9ffe4ded 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "dzm1098/website",
+    "data-repo": "dzm1098/dzm1098.github.io",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
